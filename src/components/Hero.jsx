1a2daba8f56c6b72ebb39ac6052fb91831946ff6
Son904511 <tusharjee18@gmail.com>
2025-07-10 11:29:54 +0530

    Implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
-import React from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   useGSAP(() => {
     const heroSplit = new SplitText(".title", {
       type: "chars,words",
@@ -30,20 +34,51 @@ const Hero = () => {
       delay: 1,
     });
 
-    gsap.timeline({
-      scrollTrigger:{
-        trigger:"#hero",
-        start:"top top",
-        end:"bottom top",
-        scrub:true,
-      }
-    })
-    .to('.right-leaf',{
-      y:200,
-    },0)
-    .to('.left-leaf',{
-      y:-200,
-    },0)
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(
+        ".right-leaf",
+        {
+          y: 200,
+        },
+        0
+      )
+      .to(
+        ".left-leaf",
+        {
+          y: -200,
+        },
+        0
+      );
+    // element to Animate,viewport value
+    // when top of video is 50% down the screen animation starts
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+    // when top of video is 120 from screen top
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+        // video stuck on screen
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []); //happen only once when the component mounts
   return (
     <>
@@ -79,6 +114,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
